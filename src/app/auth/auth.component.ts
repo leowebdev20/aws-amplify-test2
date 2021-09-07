743ebbaa89d537f5d352beddd9143d975dedc4fb
Leonardo Seren <leonardo.seren@primoround.com>
2021-09-07 10:21:56 +0200

    mod 4

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,25 +1,33 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
-
+import {
+  onAuthUIStateChange,
+  CognitoUserInterface,
+  AuthState,
+} from '@aws-amplify/ui-components';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent {
   title = 'amplify-angular-auth';
   user: CognitoUserInterface | undefined;
   authState!: AuthState;
 
-  constructor(private ref: ChangeDetectorRef) {}
+  constructor(private ref: ChangeDetectorRef, private router: Router) {}
 
   ngOnInit() {
     onAuthUIStateChange((authState, authData) => {
       this.authState = authState;
       this.user = authData as CognitoUserInterface;
       this.ref.detectChanges();
-    })
+    });
+  }
+
+  gotoTodo() {
+    this.router.navigate(['/todo']);
   }
 
   ngOnDestroy() {
